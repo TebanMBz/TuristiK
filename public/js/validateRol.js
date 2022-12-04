e88afe27f78ac2df5ffc014e3860071ff10bbc8d
tefa9802 <alvarezsepulvedaestefania@gmail.com>
2022-12-03 21:36:16 -0500

    Validar Nombre

--- public/js/validateRol.js
+++ public/js/validateRol.js
@@ -1,3 +1,22 @@
+//VALIDATE JUST LETTERS
+function soloLetras(e) {
+    var key = e.keyCode || e.which,
+      tecla = String.fromCharCode(key).toLowerCase(),
+      letras = " áéíóúabcdefghijklmnñopqrstuvwxyz",
+      especiales = [8, 37, 39, 46],
+      tecla_especial = false;
+
+    for (var i in especiales) {
+      if (key == especiales[i]) {
+        tecla_especial = true;
+        break;
+      }
+    }
+
+    if (letras.indexOf(tecla) == -1 && !tecla_especial) {
+      return false;
+    }
+  }
 
 $('#btnCancelarRol').click(function(){
     Swal.fire({
@@ -12,6 +31,18 @@ $('#btnCancelarRol').click(function(){
 //VALIDATE CREATE ROL--------------------------
 
 $('#btnCreateRol').click(function() {
+
+    if($("#RolName").val().length < 1) {
+        Swal.fire({
+            icon: 'warning',
+            title: 'El campo Nombre es obligatorio.',
+            showConfirmButton: false,
+            timer: 4500
+            
+        }).then(function(){
+            window.location='/CreateRol'
+        })
+    }
 
     //VALIDATE NAME
     if($("#RolName").val().length < 1) {
